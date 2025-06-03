e75c92a071a2a3900a67f63efaa93fa4783809d6
AnnurAfgoni <afgoniannur@gmail.com>
2025-06-03 16:49:32 +0800

    change columns id to uuid

--- src/db/models/users.py
+++ src/db/models/users.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, String, Boolean
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
 
 from db.base_class import Base
 
@@ -6,7 +8,7 @@ from db.base_class import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
